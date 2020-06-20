eabceb2f0748b1f4886a91e39a8a1999c93af9c5
afsal <afsal@symphonyai.com>
2020-06-20 15:59:15 +0530

    adsense added

---
+++ precache-manifest.9383cb897287f39c7e2bfb1f83286e58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b78e1c604791bddae319",
+    "url": "/static/css/main.c90abf10.chunk.css"
+  },
+  {
+    "revision": "b78e1c604791bddae319",
+    "url": "/static/js/main.b78e1c60.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c88c1e8a08a8338d503a26936bc8b368",
+    "url": "/index.html"
+  }
+];
